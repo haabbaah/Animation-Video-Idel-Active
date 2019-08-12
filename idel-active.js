ddf74523f6b50d1505c4fc22825faf4e4f0e36c1
Ilya Plank <ilplank@yandex.ru>
2019-08-12 13:24:11 +0500

    with_class

--- idel-active.js
+++ idel-active.js
@@ -1,10 +1,52 @@
-function VideoAnimation(option) {
+
+
+	class IdelActiveAnimation {
+		constructor({ //значения по умолчанию
+			selector: selector = 'video',
+			startTime: startTime = 0,
+		}) {
+			this.videos = document.querySelectorAll(selector);
+			this.startTime = startTime;
+
+			for (const video of this.videos) {
+				video.addEventListener('loadedmetadata', (event) => {
+					this.animateIdel(video);
+				}, false);
+				video.addEventListener('touchstart', (event) => {
+					this.animateActive(video);
+				}, false);
+				video.addEventListener('ended', (event) => {
+					this.animateIdel(video);
+				}, false);
+
+				this.animateIdel(video);
+			}
+		}
+
+		animateIdel(video) {
+			video.currentTime = this.startTime;
+			video.play();
+		}
+
+		animateActive(video) {
+			video.classList.remove('animate');
+			void video.offsetWidth; // reflow hack
+			video.classList.add('animate');
+			video.currentTime = 0.1;
+			video.play();
+		}
+	}
+
+
+
+	/* 
+
 
 	//Полифил для requestAnimationFrame
 	(function () {
-		var lastTime = 0;
-		var vendors = ['ms', 'moz', 'webkit', 'o'];
-		for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		let lastTime = 0;
+		let vendors = ['ms', 'moz', 'webkit', 'o'];
+		for (let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
 			window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
 			window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
 				window[vendors[x] + 'CancelRequestAnimationFrame'];
@@ -12,9 +54,9 @@ function VideoAnimation(option) {
 
 		if (!window.requestAnimationFrame)
 			window.requestAnimationFrame = function (callback, element) {
-				var currTime = new Date().getTime();
-				var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-				var id = window.setTimeout(function () {
+				let currTime = new Date().getTime();
+				let timeToCall = Math.max(0, 16 - (currTime - lastTime));
+				let id = window.setTimeout(function () {
 						callback(currTime + timeToCall);
 					},
 					timeToCall);
@@ -30,42 +72,38 @@ function VideoAnimation(option) {
 	//Полифил для requestAnimationFrame end
 
 
-	var video = document.getElementById(option.selictor);
-	var startTime = option.startTime;
-	var i = true;
-	var flagAnimation = 0;
+	function VideoAnimation(option) {
 
+		let video = document.getElementById(option.selector);
+		let startTime = option.startTime;
+		let i = true;
 
-	requestAnimationFrame(animateIdel);
+		requestAnimationFrame(animateIdel);
 
-	function animateIdel() {
-		if (i) {
-			i = false;
-		} else {
-			video.currentTime = startTime;
-			setTimeout(function () {
-				video.play();
-			}, 0);
-			i = true;
-			requestAnimationFrame(animateIdel);
+		function animateIdel() {
+			if (i) {
+				i = false;
+			} else {
+				video.currentTime = startTime;
+				setTimeout(function () {
+					video.play();
+				}, 0);
+				i = true;
+				requestAnimationFrame(animateIdel);
+			}
 		}
-	}
 
-	function animateActive() {
-		if (flagAnimation === 0) {
-			video.setAttribute('class', 'animate');
-			flagAnimation++;
-		} else {
-			video.setAttribute('class', 'animate2');
-			flagAnimation = 0;
+		function animateActive() {
+			video.classList.remove('animate');
+			void video.offsetWidth; // reflow hack
+			video.classList.add('animate');
+			video.currentTime = 0.1;
+			video.play();
 		}
-		video.currentTime = 0.1;
-		video.play();
-	}
 
 
-	video.addEventListener('loadedmetadata', animateIdel, false);
-	video.addEventListener('click', animateActive, false);
-	video.addEventListener('ended', animateIdel, false);
+		video.addEventListener('loadedmetadata', animateIdel, false);
+		video.addEventListener('click', animateActive, false);
+		video.addEventListener('ended', animateIdel, false);
 
-}
+	} */
